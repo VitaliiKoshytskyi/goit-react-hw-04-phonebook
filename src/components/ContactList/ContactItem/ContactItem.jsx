f9ed9d70fd738a0984c75ad007922daed3dab65d
Vitalii Koshytskyi <115081482+VitaliiKoshytskyi@users.noreply.github.com>
2023-02-27 09:08:47 +0200

    updated markup

--- src/components/ContactList/ContactItem/ContactItem.jsx
+++ src/components/ContactList/ContactItem/ContactItem.jsx
@@ -1,25 +1,23 @@
+import PropTypes from 'prop-types';
 
-import PropTypes from 'prop-types'
+import css from './ContactItem.module.css';
 
-import css from './ContactItem.module.css'
+const ContactItem = ({ id, name, number, deleteContactHandler }) => {
+  return (
+    <li className={css.item}>
+      <p>
+        {name} : {number}
+      </p>
+      <button onClick={() => deleteContactHandler(id)}>Delete</button>
+    </li>
+  );
+};
 
-
-const ContactItem = ({id,name,number,deleteContactHandler}) => {
-    
-    return (
-        <li className={css.item} >
-            <p>{name} : {number}</p>
-            <button onClick={() => deleteContactHandler(id)}>Delete</button>
-        </li>
-    )
-}
-
-export default ContactItem
-
+export default ContactItem;
 
 ContactItem.propTypes = {
-    id:PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
   number: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
- deleteContactHandler:PropTypes.func.isRequired
+  deleteContactHandler: PropTypes.func.isRequired,
 };
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,31 +1,25 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
+import ContactItem from './ContactItem/ContactItem';
+import css from './ContactList.module.css';
 
+const ContactList = ({ items = [], deleteContactHandler }) => {
+  return (
+    <ul className={css.list}>
+      {items.map(item => (
+        <ContactItem
+          key={item.id}
+          id={item.id}
+          name={item.name}
+          number={item.number}
+          deleteContactHandler={deleteContactHandler}
+        />
+      ))}
+    </ul>
+  );
+};
 
-import ContactItem from "./ContactItem/ContactItem";
-import css from './ContactList.module.css'
-
-const ContactList = ({items =[], deleteContactHandler}) => {
-    
-
-    return (
-        
-        <ul className={css.list}>
-            {items.map(item => (
-                <ContactItem
-                    key={item.id}
-                    id={item.id}
-                    name={item.name}
-                    number={item.number}
-                    deleteContactHandler={ deleteContactHandler} />)
-                )}
-          </ul>
-        
-        
-    )
-}
-
-export default ContactList
+export default ContactList;
 
 ContactList.propTypes = {
   items: PropTypes.arrayOf(
